1b7fe95bed56e5239a58a85e832637385a68f075
Thicam <15014743+ThibaultCam@users.noreply.github.com>
2025-12-03 17:09:16 +0100

    fill films.html

--- src/app/core/models/film.ts
+++ src/app/core/models/film.ts
@@ -7,4 +7,5 @@ export interface Film {
   releaseDate: string;
   durationMinutes: number;
   genres: Array<Genre>;
+  imageUrl?: string | null;
 }
--- src/app/features/films/films.ts
+++ src/app/features/films/films.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Film } from '../../core/models/film';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { Genre } from '../../core/models/genre';
 
 @Component({
   selector: 'app-films',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, CommonModule ],
   templateUrl: './films.html',
   styleUrl: './films.scss',
 })
@@ -18,4 +22,7 @@ export class Films implements OnInit {
     console.log(this.films);
   }
 
+  inlineGenres(genres: Genre[]): string {
+    return genres.map(genre => genre.name).join(', ');
+  }
 }
