d75c91e2d5526a7e935dc6e7665600e24301bf4e
Thicam <15014743+ThibaultCam@users.noreply.github.com>
2025-12-03 17:32:41 +0100

    update header

--- src/app/shared/modules/materialModule.ts
+++ src/app/shared/modules/materialModule.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule
   ]
 })
 export class MaterialModule {}
